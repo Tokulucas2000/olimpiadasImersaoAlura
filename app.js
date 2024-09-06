d2eae85f69af24b2eee126e829bf70701a87e84d
Tokulucas2000 <47503175+Tokulucas2000@users.noreply.github.com>
2024-09-06 20:07:20 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+function pesquisar()
+{
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    if(campoPesquisa == "")
+    {
+        section.innerHTML = "<p>Nada foi encontrado!</p>"
+        return;
+    }
+
+    campoPesquisa.toLowerCase();
+
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tag = "";
+
+        for(let dado of dados)
+        {
+            titulo = dado.titulo.toLowerCase();
+            descricao = dado.descricao.toLowerCase();
+            tag = dado.tags.toLowerCase(); 
+            if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tag.includes(campoPesquisa))
+            {
+                resultados += `
+                <div class="item-resultado">
+                        <h2>
+                            <a href="#" target="_blank">${dado.titulo}</a>
+                        </h2>
+                        <p class="descricao-meta">${dado.descricao}</p>
+                            <a href="${dado.link}" target="_blank">Mais Informações</a>
+                </div>`
+            }
+        }
+        if(!resultados)
+        {
+            resultados = `<p>${campoPesquisa} não foi encontrado!</p>`
+            return;
+        }
+
+    section.innerHTML = resultados;
+}
+
---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, também conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags : "skate skatista fadinha"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e força tetetetetetetetetetetetetetetetetetetetetyaa.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza", // Substitua por um link válido para Beatriz Souza
+        tags : "judo judoca ameno"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou diversas medalhas em competições internacionais.tetetetetetetetetet",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade", // Substitua por um link válido para Rebeca Andrade
+        tags : "ginasta ginastica ouro"
+    }
+];
